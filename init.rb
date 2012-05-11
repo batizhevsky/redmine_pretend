f882b86a6005f9d444ce1bcbb0945a9982ca9fdd
Leonid Batizhevsky <the.leonko@gmail.com>
2012-05-11 12:17:11 +0400

    plugint skeleton

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_pretend do
+  name 'Redmine Pretend plugin'
+  author 'Leonid Batizhevsky'
+  description 'Plugin to pretend selected user'
+  version '0.0.1'
+  url 'https://github.com/leonko/redmine_pretend'
+  author_url 'https://twitter.com/#!/batizhevsky'
+end
