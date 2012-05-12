de6e5d0a4cd0d98960892f88ff9688e882d48f7c
Leonid Batizhevsky <the.leonko@gmail.com>
2012-05-12 13:34:28 +0400

    First work version

--- init.rb
+++ init.rb
@@ -1,4 +1,11 @@
 require 'redmine'
+require 'dispatcher'
+
+require 'application_controller_patch'
+require 'admin_controller_patch'
+require_dependency 'redmine_pretend/hooks'
+
+RAILS_DEFAULT_LOGGER.info 'Starting Pretend plugin for RedMine'
 
 Redmine::Plugin.register :redmine_pretend do
   name 'Redmine Pretend plugin'
@@ -6,5 +13,11 @@ Redmine::Plugin.register :redmine_pretend do
   description 'Plugin to pretend selected user'
   version '0.0.1'
   url 'https://github.com/leonko/redmine_pretend'
-  author_url 'https://twitter.com/#!/batizhevsky'
+
+  requires_redmine :version_or_higher => '0.9.0'
 end
+
+#Dispatcher.to_prepare do
+  #ApplicationController.send(:include, Patches::ApplicationControllerPatch)
+  #AdminController.send(:include, Patches::AdminControllerPatch)
+#end
